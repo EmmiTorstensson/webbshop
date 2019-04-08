57e637b9546a43332841c446b341b9329c0d945e
emmitorstensson <emmitorstensson@hotmail.com>
2019-04-08 21:05:36 +0200

    sma andringar

--- script-orderpage.js
+++ script-orderpage.js
@@ -8,26 +8,46 @@ $(document).ready(function(){
     console.log(typeof shoppingItem);
 
     for (let i = 0; i < shoppingItem.length; i++) {
-      $("#cart-update").after('<tr><td><img class="table-img" src="' + shoppingItem[i].img + '"></td><td>' + shoppingItem[i].title + '</td><td id="qty">' + qty + '</td><td>$' + shoppingItem[i].price +'</td></tr>')
-
-          ;
-
-}
-
-
-//Funkar ej
-
-$("#clear-cart").click(function() {
-    $("td").remove();
-    localStorage.removeItem("itemsAdded")
-  });
-
-
-/*
-function updateCartTotal(){
-let total = 0;
-total = total +  shoppingItem[i].price;
-document.getElementsByClassName("cart-total-price").innerText = "$" + total ;
-}
-*/
-});
+        $("#cart-update").after('<tr><td><img class="table-img" src="' + shoppingItem[i].img + '"></td><td>' + shoppingItem[i].title + '</td><td id="qty">' + qty + '</td><td>$' + shoppingItem[i].price +'</td></tr>')
+    }
+    
+    
+    //Funkar ej
+    
+    $("#clear-cart").click(function() {
+        $("td").remove();
+        localStorage.removeItem("itemsAdded")
+    });
+    
+    $("#btn-purchase").click(function(){
+        validate()  
+    })
+    
+    function validate() {
+        
+        if( document.myForm.fullname.value == "" ) {
+            alert( "Please provide your name!" );
+            document.myForm.fullname.focus() ;
+            return false;
+        }
+        if( document.myForm.EMail.value == "" ) {
+            alert( "Please provide your Email!" );
+            document.myForm.EMail.focus() ;
+            return false;
+        }
+        if( document.myForm.zip.value == "" || isNaN( document.myForm.zip.value ) ||
+        document.myForm.zip.value.length != 5 ) {
+            
+            alert( "Please provide a zip in the format #####." );
+            document.myForm.Zip.focus() ;
+            return false;
+        }
+        if( document.myForm.city.value == "-1" ) {
+            alert( "Please provide your country!" );
+            return false;
+        }
+        return( true );
+        
+    }
+    
+}); // READY
